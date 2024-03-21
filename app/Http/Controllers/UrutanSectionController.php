be457802e12bb7f06779ef189f9d9cf7063d02c7
AlyDap <ferdynan.jr@gmail.com>
2024-03-21 10:46:19 +0700

    lanjut UrutanSectionController

--- app/Http/Controllers/UrutanSectionController.php
+++ app/Http/Controllers/UrutanSectionController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Section_Gambar;
+use App\Models\Section_Gbr_Hdg_Prgf;
+use App\Models\Section_Peta;
+use App\Models\Section_Slideshow;
+use App\Models\Section_Tulisan;
 use App\Models\Urutan_Section;
 use Illuminate\Http\Request;
 
@@ -13,44 +17,68 @@ class UrutanSectionController extends Controller
   // Mengambil semua data dari tabel Urutan_Section
   // $urutanSection = Urutan_Section::all();
   $urutanSection = Urutan_Section::orderBy('urutan_section')->get();
+  //id_section= foreign key
+  $sectionGambar = Section_Gambar::all()->keyBy('id_section');
+  $sectionPeta = Section_Peta::all()->keyBy('id_section');
+  $sectionTulisan = Section_Tulisan::all()->keyBy('id_section');
+  $sectionSlideshow = Section_Slideshow::all()->keyBy('id_section');
+  $sectionGHP = Section_Gbr_Hdg_Prgf::all()->keyBy('id_section');
   // Menyiapkan array untuk menyimpan hasil
   $sectionData = [];
+  $col = '<div class="col-12 m-0 p-0">';
+  $penutupDiv = '</div>';
 
   // Melakukan iterasi melalui setiap baris data
   foreach ($urutanSection as $section) {
 
    // section_slideshows
    if ($section->jenis_section === 'slide show') {
+    $sectionData[] = $col;
     $sectionData[] = '<h1>' . $section->deskripsi_section . '</h1>';
+    $sectionData[] = $penutupDiv;
 
 
     // section_petas
    } elseif ($section->jenis_section === 'peta') {
-    $sectionData[] = '<a href="#">' . $section->deskripsi_section . '</a>';
+    $sectionPetaId = $sectionPeta[$section->id] ?? null;
+    if ($sectionPetaId) {
+     $sectionData[] = $col;
+     $sectionData[] = $sectionPetaId->url_peta;
+     $sectionData[] = $penutupDiv;
+    }
 
 
     // section_gambars
    } elseif ($section->jenis_section === 'gambar full') {
     // $sectionGambar = Section_Gambar::where('id_section', $section->id)->where('jenis_section', 'gambar full')->first();
-    $sectionGambar = Section_Gambar::where('id_section', $section->id)->first();
-    if ($sectionGambar) {
-     $sectionData[] = '<img src="' . $sectionGambar->file_gambar . '" alt="' . $sectionGambar->file_gambar . '">';
+    // $sectionGambarId = Section_Gambar::where('id_section', $section->id)->first();
+
+    // Mengambil data Section_Gambar menggunakan referensi langsung ke Urutan_Section
+    $sectionGambarId = $sectionGambar[$section->id] ?? null;
+    if ($sectionGambarId) {
+     $sectionData[] = $col;
+     $sectionData[] = '<img class="section-gambar-besar" src="/img/landing_page/' . $sectionGambarId->file_gambar . '" alt="' . $sectionGambarId->file_gambar . '">';
+     $sectionData[] = $penutupDiv;
     }
 
 
     // section_tulisans
    } elseif ($section->jenis_section === 'tulisan dengan bg warna full') {
-    $sectionData[] = '<button>' . $section->deskripsi_section . '</button>';
+    $sectionData[] = $col;
+    $sectionData[] = '<div class="bg-primary d-flex align-items-center text-center justify-content-center fs-2 py-5">' . $section->deskripsi_section . '</div>';
+    $sectionData[] = $penutupDiv;
 
 
     // section_gbrh_hdg_prgfs
    } elseif ($section->jenis_section === 'gambar heading paragraf') {
+    $sectionData[] = $col;
     $sectionData[] = '<p>' . $section->deskripsi_section . '</p>';
+    $sectionData[] = $penutupDiv;
    }
 
 
    // garis
-   $sectionData[] = '<hr>';
+   // $sectionData[] = '<hr>';
   }
 
 
--- resources/views/section.blade.php
+++ resources/views/section.blade.php
@@ -6,12 +6,44 @@
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
  <title>Section</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+ <style>
+  /* Mengatur lebar dan tinggi elemen iframe */
+  iframe {
+   width: 100%;
+   /* ganti dengan lebar yang diinginkan */
+   height: 300px;
+   /* ganti dengan tinggi yang diinginkan */
+  }
+
+  .section-gambar-besar {
+   width: 100%;
+   height: 400px;
+   object-fit: cover;
+   object-position: center;
+  }
+
+  /* .section-tulisan-bg {
+   height: auto;
+  } */
+ </style>
 </head>
 
 <body>
- @foreach ($sectionData as $data)
-  {!! $data !!}
- @endforeach
+ <div class="container-fluid m-0">
+  <div class="row">
+
+   @foreach ($sectionData as $data)
+    {!! $data !!}
+   @endforeach
+
+  </div>
+ </div>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
